9752a2ca9b4373c9c172f5a4c95f4e4359f784ca
Ludovic <ludovicaubut@hotmail.com>
2017-02-11 11:37:59 -0500

    Ai ajouté des sprites pour les machines.

--- Assets/Scripts/Machine.cs
+++ Assets/Scripts/Machine.cs
@@ -1,13 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Machine : MonoBehaviour {
 
 	public string buttonName;
 	public int objectiveRate;
+	public int perHowManySeconds;
 	private List<float> tapTimes;
-	public float tapsPerTenSecond { get; private set; }
+	private Text caption;
+	public float tapsPerXSecond { get; private set; }
 	public float power { get; private set; }
 
 	void Start() {
@@ -15,6 +18,7 @@ public class Machine : MonoBehaviour {
 		if (objectiveRate == 0) {
 			objectiveRate = 1;
 		}
+		caption = this.GetComponentInChildren<Text> ();
 	}
 
 	void Update () {
@@ -22,11 +26,12 @@ public class Machine : MonoBehaviour {
 			tapTimes.Add (Time.time);
 		}
 		if (tapTimes.Count > 0) {
-			if (tapTimes[0] < (Time.time - 2.0f)) {
+			if (tapTimes[0] < (Time.time - perHowManySeconds)) {
 				tapTimes.RemoveAt(0);
 			}
 		}
-		tapsPerTenSecond = tapTimes.Count;
-		power = (tapsPerTenSecond / (float)objectiveRate) * 100;
+		tapsPerXSecond = tapTimes.Count;
+		power = (tapsPerXSecond / (float)objectiveRate);
+		caption.text = power.ToString("P0");
 	}
 }
